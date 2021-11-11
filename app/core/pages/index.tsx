ea94140edb485eb5da3c54f1221b717e4b10ab1d
Ben Honeywill <benhoneywill@gmail.com>
2021-11-11 17:48:18 +0000

    test

--- app/core/pages/index.tsx
+++ app/core/pages/index.tsx
@@ -19,7 +19,7 @@ const HomePage: BlitzPage = () => {
 
   return (
     <>
-      <h1>Home (preview)</h1>
+      <h1>Home (preview test)</h1>
       <Link href={Routes.NewPostPage()} passHref>
         <Button as="a">New post</Button>
       </Link>
