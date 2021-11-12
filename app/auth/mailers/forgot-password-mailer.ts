ca886e3f6e5c226c8dbda3c8816ca4eaacd14d3f
Ben Honeywill <benhoneywill@gmail.com>
2021-11-12 23:09:00 +0000

    origin env var

--- app/auth/mailers/forgot-password-mailer.ts
+++ app/auth/mailers/forgot-password-mailer.ts
@@ -1,21 +1,22 @@
 import type { User } from "db";
 
 import { postmark } from "integrations/postmark";
-import { ORIGIN } from "app/core/constants/env";
+
+const APP_ORIGIN = process.env.BLITZ_PUBLIC_APP_ORIGIN;
 
 type ResetPasswordOptions = {
   token: string;
 };
 
 export const sendForgotPasswordEmail = (user: User, { token }: ResetPasswordOptions) => {
-  const resetUrl = `${ORIGIN}/reset-password?token=${token}`;
+  const resetUrl = `${APP_ORIGIN}/reset-password?token=${token}`;
 
   return postmark.sendEmailWithTemplate({
     From: "app@hikerherd.com",
     To: user.email,
     TemplateAlias: "password-reset",
     TemplateModel: {
-      product_url: ORIGIN,
+      product_url: APP_ORIGIN,
       product_name: "hikerherd",
       name: user.username,
       action_url: resetUrl,
