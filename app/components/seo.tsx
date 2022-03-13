77586350aa1f087a6fa58f1acf640b10c1dfb386
Ben Honeywill <benhoneywill@gmail.com>
2022-03-13 22:48:38 +0000

    update seo tagline

--- app/components/seo.tsx
+++ app/components/seo.tsx
@@ -10,9 +10,8 @@ type SeoProps = {
 const DEFAULT_TITLE = "hikerherd | the trail online";
 
 const DEFAULT_DESCRIPTION = `
-  The best online hiking and backpacking community, Join hikerherd now and start using
-  our free online tools designed to help you minimize your pack weight and make your
-  next adventure the best it can be!
+  Join hikerherd now and start using our free online tools designed to help you minimize
+  your pack weight and make your next adventure the best it can be!
 `;
 
 const Seo: FC<SeoProps> = ({ title, description }) => (
