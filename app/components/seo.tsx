a97f280b31ca32169c553ec05346dc863646a751
Ben Honeywill <benhoneywill@gmail.com>
2022-03-28 21:42:50 +0100

    add seo tags

--- app/components/seo.tsx
+++ app/components/seo.tsx
@@ -18,6 +18,35 @@ const Seo: FC<SeoProps> = ({ title, description }) => (
   <Head>
     <title>{title ? `${title} | hikerherd` : DEFAULT_TITLE}</title>
     <meta name="description" content={description || DEFAULT_DESCRIPTION} />
+
+    <meta
+      property="og:title"
+      content={title ? `${title} | hikerherd` : DEFAULT_TITLE}
+    />
+    <meta
+      property="og:description"
+      content={description || DEFAULT_DESCRIPTION}
+    />
+    <meta
+      property="og:image"
+      content={process.env.BLITZ_PUBLIC_APP_ORIGIN + "/hikerherd.png"}
+    />
+    <meta property="og:type" content="website" />
+    <meta
+      name="og:title"
+      content={title ? `${title} | hikerherd` : DEFAULT_TITLE}
+    />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta
+      name="twitter:image"
+      content={process.env.BLITZ_PUBLIC_APP_ORIGIN + "/hikerherd.png"}
+    />
+    <meta name="twitter:creator" content="@benhoneywill" />
+    <meta
+      name="twitter:description"
+      content={description || DEFAULT_DESCRIPTION}
+    />
   </Head>
 );
 
